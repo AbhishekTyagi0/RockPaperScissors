b3f2152220760623c47fc4addc4b19c02c86bc08
Abhishek Tyagi <abhishektyagigi4310@gmail.com>
2023-09-24 20:49:08 +0530

    added more program
    Now the program will compare the system choice and userInput to announce the winner

--- index.js
+++ index.js
@@ -1,21 +1,52 @@
 
 
 let systemInput;
+
 function computerChoice() {
-    let weaponChoice = ["Rock","Paper","Scissor"];
-    systemInput = Math.floor(Math.random()*weaponChoice.length);
+    let weaponChoice = ["Rock", "Paper", "Scissor"];
+    systemInput = Math.floor(Math.random() * weaponChoice.length);
     let choice = weaponChoice[systemInput];
     return choice;
 }
-console.log(computerChoice());
 
 
 let userInput = prompt("Enter your Weapon of Choice: ");
 
 
-function RPS(systemInput,userInput) {
-    
-    if(userInput === systemInput){
+console.log(userInput);
+
+function RPS(system, user) {
+
+    if (system === user) {
         console.log("match Tie, Try again");
+    } else if (system === "Rock" && user === "Scissor") {
+        console.log("Computer wins, you loose");
+
+    }
+    else if (system === "Paper" && user === "Rock") {
+
+        console.log("Computer wins, you loose");
+    } else if (system === "Scissor" && user === "Paper") {
+
+        console.log("Computer wins, you loose");
+
+    } else if (system === "Scissor" && user === "Rock") {
+        console.log("You are a winner, keep going");
+
+    }
+    else if (system === "Paper" && user === "Scissor") {
+
+        console.log("You are a winner, keep going");
+    }
+    else if (system === "Rock" && user === "Paper") {
+        console.log("You are a winner, keep going");
+    }
+    else {
+        console.log("Incorrect input, try again")
     }
 }
+
+let computer = computerChoice();
+console.log(computer);
+
+console.log(RPS(computer, userInput));
