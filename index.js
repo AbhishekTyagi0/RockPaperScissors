0574e03af33086ceef02cfd30aed395603de731d
Abhishek Tyagi <abhishektyagigi4310@gmail.com>
2023-10-02 19:24:21 +0530

    Add logic to the DOM

--- index.js
+++ index.js
@@ -6,41 +6,53 @@ function computerChoice() {
     let weaponChoice = ["Rock", "Paper", "Scissor"];
     systemInput = Math.floor(Math.random() * weaponChoice.length);
     let choice = weaponChoice[systemInput].toUpperCase();
+    const systemWeapon = document.querySelector('#systeminput');
+    systemWeapon.innerText = `Computers weapon is: ${choice}`;
+
+
     return choice;
 }
 
 
-let userScore=0,computerScore=0;
 
 function RPS(system, user) {
 
     if (system === user) {
-        console.log("match Tie, Try again");
+
+        const roundWinner = document.querySelector('.round');
+        roundWinner.innerText = "match Tie, Try again";
+
     } else if (system === "ROCK" && user === "SCISSOR") {
-        console.log(`Computer wins, ${system} beats ${user}`);
+        const roundWinner = document.querySelector('.round');
+        roundWinner.innerText = `Computer wins, ${system} beats ${user}`;
         computerScore += 1;
     }
     else if (system === "PAPER" && user === "ROCK") {
 
-        console.log(`Computer wins, ${system} beats ${user}`);
+        const roundWinner = document.querySelector('.round');
+        roundWinner.innerText = `Computer wins, ${system} beats ${user}`;
         computerScore += 1;
     } else if (system === "SCISSOR" && user === "PAPER") {
 
-        console.log(`Computer wins, ${system} beats ${user}`);
+        const roundWinner = document.querySelector('.round');
+        roundWinner.innerText = `Computer wins, ${system} beats ${user}`;
         computerScore += 1;
 
     } else if (system === "SCISSOR" && user === "ROCK") {
-        console.log(`You are a winner,${user} beats ${system}`);
+        const roundWinner = document.querySelector('.round');
+        roundWinner.innerText = `You are a winner,${user} beats ${system}`;
         userScore += 1;
 
     }
     else if (system === "PAPER" && user === "SCISSOR") {
 
-        console.log(`You are a winner,${user} beats ${system}`);
+        const roundWinner = document.querySelector('.round');
+        roundWinner.innerText = `You are a winner,${user} beats ${system}`;
         userScore += 1;
     }
     else if (system === "ROCK" && user === "PAPER") {
-        console.log(`You are a winner,${user} beats ${system}`);
+        const roundWinner = document.querySelector('.round');
+        roundWinner.innerText = `You are a winner,${user} beats ${system}`;
         userScore += 1;
 
     }
@@ -54,43 +66,65 @@ function RPS(system, user) {
 
 let userWeapon;
 
-const btn1 = document.getElementById('btn1').addEventListener('click',function(){
+const btn1 = document.getElementById('btn1').addEventListener('click', function () {
     userWeapon = 'Rock';
     userWeapon = userWeapon.toUpperCase();
+    const userInput = document.querySelector('#userinput');
+    userInput.innerText = `Your weapon is: ${userWeapon}`;
     console.log(userWeapon);
     game();
-    console.log(userScore,computerScore);
+
+
+
+
+
 
 });
-const btn2 = document.getElementById('btn2').addEventListener('click',function(){
+const btn2 = document.getElementById('btn2').addEventListener('click', function () {
     userWeapon = 'Paper';
     userWeapon = userWeapon.toUpperCase();
+    const userInput = document.querySelector('#userinput');
+    userInput.innerText = `Your weapon is: ${userWeapon}`;
     console.log(userWeapon);
     game();
-    console.log(userScore,computerScore);
 
 });
-const btn3 = document.getElementById('btn3').addEventListener('click',function(){
+const btn3 = document.getElementById('btn3').addEventListener('click', function () {
     userWeapon = 'Scissor';
     userWeapon = userWeapon.toUpperCase();
+    const userInput = document.querySelector('#userinput');
+    userInput.innerText = `Your weapon is: ${userWeapon}`;
+
     console.log(userWeapon);
     game();
-    console.log(userScore,computerScore);
 
 
 });
 
 function game() {
 
-    let user = userWeapon;
-    let computer = computerChoice();
-    RPS(computer, user);
-    let gameResult = computerScore > userScore ? "Computer is a winner" : computerScore === userScore ? "Match tie" : "You are winner";
-return gameResult;
+    let userScore = 0, computerScore = 0;
 
+    // while (userScore < 5 && computerScore < 5) {
+
+        let user = userWeapon;
+        let computer = computerChoice();
+        RPS(computer, user);
+        let gameResult = computerScore > userScore ? "Computer is a winner" : computerScore === userScore ? "Match tie" : "You are winner";
+
+        const gameWinner = document.querySelector('.result');
+        gameWinner.innerText = `Match Result: ${gameResult}`;
+
+
+
+        
+    // }
+    document.querySelector('.score').innerText = `Your Score is: ${userScore}\n Computer Score is: ${computerScore}`;
 
 }
-
 game();
+
+
+
 
 
